e8e382d2369edccaf9c0027b1787f64701b484f0
554393109 <554393109@qq.com>
2020-02-26 15:49:56 +0800

    添加单元测试

---
+++ NetFramework/UnifiedPaySDK.UnitTest/ExecuteTest.cs
@@ -0,0 +1,56 @@
+﻿namespace UnifiedPaySDK.UnitTest
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using UnifiedPaySDK.Request;
+    using UnifiedPaySDK.Response;
+
+    [TestClass]
+    public class ExecuteTest
+    {
+        [TestMethod]
+        public void Pay_Test()
+        {
+            var request = new PayRequest();
+            var response = new PayResponse();
+
+            Assert.AreEqual<string>("success", "success");
+        }
+
+        [TestMethod]
+        public void OrderQuery_Test()
+        {
+            var request = new OrderQueryRequest();
+            var response = new OrderQueryResponse();
+
+            Assert.AreEqual<string>("success", "success");
+        }
+
+        [TestMethod]
+        public void Refund_Test()
+        {
+            var request = new RefundRequest();
+            var response = new RefundResponse();
+
+            Assert.AreEqual<string>("success", "success");
+        }
+
+        [TestMethod]
+        public void RefundQuery_Test()
+        {
+            var request = new RefundQueryRequest();
+            var response = new RefundQueryResponse();
+
+            Assert.AreEqual<string>("success", "success");
+        }
+
+        [TestMethod]
+        public void Cancel_Test()
+        {
+            var request = new CancelRequest();
+            var response = new CancelResponse();
+
+            Assert.AreEqual<string>("success", "success");
+        }
+    }
+}
---
+++ NetFramework/UnifiedPaySDK.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnifiedPaySDK.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("UnifiedPaySDK.UnitTest")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5c312094-4638-4871-9a45-f32730a90d09")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
